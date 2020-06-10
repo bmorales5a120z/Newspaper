95832fef28c6dfdc61701aa03959701552d526be
Bosco Moises Morales <bmoises.morales@gmail.com>
2020-06-10 00:57:28 +0000

    bish

--- paper.js
+++ paper.js
@@ -9,46 +9,44 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies = ["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var isThereKryptonite
 
-funtion Who_Wins(is-there-kryptonite,enemy_name)
-{
-if (!is-there-kryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
-}
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
-}
+funtion WhoWins(isThereKryptonite,enemyName){
+    if (!isThereKryptonite){
+         return "Superman beats " + enemyName + ", of course";
+    }   
+    else{
+        return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
     }
+}
 
-for (var i=0;i<enemies.length;i++)
-{
-    var is_there_kryptonite:
+for (var i = 0 ; i < enemies.length ; i++) {
     if (i%2=== 0) {
-        is_there_kryptonite = true;
+        isThereKryptonite = true;
     } else {
-        is_there_kryptonite = false;
+        isThereKryptonite = false;
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
+    console.log(WhoWins(is_there_kryptonite,enemies[i]));
 }
 
-function HowAttractedIsLoisLaneToMe ()
+function howAttractedIsLoisLaneToMe ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
     return Math.floor((Mat.random()*10)+1);
 }
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+consol.log( howAttractedIsLoisLaneToMe ()) ;
 
 var clarkKent =true;
-var superman false;
+var superman = false;
 
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
-        clarkKent=false;
-        superman=true;
+
+    var phoneBoothQuickChange = Math.random();
+
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
         console.log("Now I'm Superman!");}}
